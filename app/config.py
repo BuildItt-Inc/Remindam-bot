dec8333ea790e90154c8c3a8e249332d6008983b
Precios Ezeigbo <preciousezeigo81@gmail.com>
2026-03-06 12:09:41 +0100

    fix: update configurations, message model and scheduler

--- app/config.py
+++ app/config.py
@@ -9,9 +9,9 @@ class Settings(BaseSettings):
     REDIS_URL: str
 
     JWT_SECRET_KEY: str
-    JWT_ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_DAYS: int
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     TWILIO_ACCOUNT_SID: str
     TWILIO_AUTH_TOKEN: str
